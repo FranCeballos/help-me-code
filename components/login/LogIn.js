20a1d04dc82f9c31471c5a80b61c555f94f40e08
Francisco Ceballos <ceballosfranciscomartin@gmail.com>
2023-06-12 18:58:40 -0300

    Home Page - Video
    - Added Hero Video

--- components/login/LogIn.js
+++ components/login/LogIn.js
@@ -4,13 +4,6 @@ import classes from "./LogIn.module.css";
 const LogIn = () => {
   return (
     <div className={classes["grid__two-columns"]}>
-      {/* <div className={classes["pic-box"]}>
-        <img
-          className={classes.pic}
-          src="/login/login.jpg"
-          alt="Log In Picture"
-        />
-      </div> */}
       <div className={classes["pic-box"]}>
         <video className={classes["video"]} autoPlay muted loop>
           <source src="/login/login1.mp4" type="video/mp4" />
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <p>Hello Next World</p>;
-}
+import { useState } from "react";
+import classes from "../components/home/HomePage.module.css";
+const HomePage = () => {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  const posterSrc = "./solid-black.jpeg";
+  const videoSrc = "./mainc3plus.mp4";
+
+  const handleVideoLoaded = () => setIsVideoLoaded(true);
+
+  return (
+    <section className={classes.home}>
+      <div className={classes["pic-box"]}>
+        {/* {!isVideoLoaded && (
+          <img className={classes.video} src={posterSrc} alt="Poster image" />
+        )} */}
+        <video
+          className={classes["video"]}
+          poster={posterSrc}
+          autoPlay
+          muted
+          loop
+        >
+          <source src="/mainc3plus.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
